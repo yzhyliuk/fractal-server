c979fb28d06f859f72d1d587917dba6cefd98f1b
y.zhyliuk <y.zhyliuk@gmail.com>
2022-07-01 15:37:34 +0300

    Clear code, delte unsued strategies

--- strategies/fallx3/const.go
+++
@@ -1,7 +0,0 @@
-package fallx3
-
-
-const (
-	stopLossMultiplier = 0.5
-	minimalProfit = 0.0085
-)
--- strategies/nadaraya-watsons/moving_average_crossover_config.go
+++
@@ -1,8 +0,0 @@
-package nadaraya_watsons
-
-import "newTradingBot/models/strategy/configs"
-
-type MovingAverageCrossoverConfig struct {
-	configs.BaseStrategyConfig
-	Multiplier int `json:"mul"`
-}
