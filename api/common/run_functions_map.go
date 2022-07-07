a9e9c575a638c8e06a0fb8b64fe8b7b567269fe2
y.zhyliuk <y.zhyliuk@gmail.com>
2022-07-07 15:42:02 +0300

    Add glide on price strategy

--- api/common/run_functions_map.go
+++ api/common/run_functions_map.go
@@ -1,9 +1,11 @@
 package common
 
 import (
+	"newTradingBot/strategies/glide_on_price"
 	"newTradingBot/strategies/mac"
 )
 
 var RunStrategy = map[int]func(userID int, rawConfig []byte) error{
 	1: mac.RunMovingAverageCrossover,
+	2: glide_on_price.RunGlideOnPrice,
 }
